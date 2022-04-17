a369a68ec92807d6ddc4e534e9d87a1424fd4f30
Tim Menzies <timm@ieee.org>
2022-04-16 20:13:47 -0400

    save

--- ako.py
+++ ako.py
@@ -9,10 +9,11 @@
 import re,tricks
 
 def good(x, nump): 
-  return (type(x)==int or type(x)== float) if nump else (type(x)==str)
+  return x=="?" and True or (
+         (type(x)==int or type(x)== float) if nump else (type(x)==str))
 
 def make(x,nump):
-  return (tricks.atom(x) if nump else x)
+  return x=="?" and x or (tricks.atom(x) if nump else x)
 
 def nump(s):   return re.search(r"^[A-Z]",s) 
 def skipp(s):  return re.search(r":$",s)
